7449fd3246ffd2777c2b0b955d970e861a638e7a
Soobin Rim <srim@sfu.ca>
2022-03-18 15:59:20 -0700

    Add more files for s3

---
+++ s3/app.py
@@ -0,0 +1,78 @@
+"""
+SFU CMPT 756
+Sample application---playlist service.
+"""
+
+# Standard library modules
+import logging
+import sys
+import time
+
+# Installed packages
+from flask import Blueprint
+from flask import Flask
+from flask import request
+from flask import Response
+
+import jwt
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import requests
+
+import simplejson as json
+
+# The application
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Playlist process')
+
+bp = Blueprint('app', __name__)
+
+db = {
+    "name": "http://cmpt756db:30002/api/v1/datastore",
+    "endpoint": [
+        "read",
+        "write",
+        "delete",
+        "update"
+    ]
+}
+
+@bp.route('/health')
+@metrics.do_not_track()
+def health():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/readiness')
+@metrics.do_not_track()
+def readiness():
+    return Response("", status=200, mimetype="application/json")
+
+
+### Modify & Add more functions here
+@bp.route('/', methods=['GET'])
+@metrics.do_not_track()
+def hello_world():
+    return ("If you are reading this in a browser, your service is "
+            "operational. Switch to curl/Postman/etc to interact using the "
+            "other HTTP verbs.")
+
+
+
+# All database calls will have this prefix.  Prometheus metric
+# calls will not---they will have route '/metrics'.  This is
+# the conventional organization.
+app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        logging.error("Usage: app.py <service-port>")
+        sys.exit(-1)
+
+    p = int(sys.argv[1])
+    # Do not set debug=True---that will disable the Prometheus metrics
+    app.run(host='0.0.0.0', port=p, threaded=True)
